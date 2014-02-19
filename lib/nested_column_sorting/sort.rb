df0138c7303ac52ab92aa3ffbb88187d3d7056c3
Shilpi89 <shilpi2189@gmail.com>
2014-02-19 04:35:37 -0800

    sorting method

--- lib/nested_column_sorting/sort.rb
+++ lib/nested_column_sorting/sort.rb
@@ -1,17 +1,16 @@
 module Nested_Column
   module Sorting   
     class Nested_Column::Sorting::Sort
-      attr_reader :table_name,:nested_column_name,:column_name
-      def initialize table_name, nested_column_name, column_name, options = {}
-        @table_name = table_name
-        @nested_column_name = nested_column_name
+      attr_reader :nested_column_data,:column_name
+      def initialize nested_column_data, column_name, options = {}
+        @nested_column_data = nested_column_data
         @column_name = column_name
         sort_nested_column
       end
 
       def sort_nested_column
-        @sorted_nested_column = JSON.parse(@table_name.@nested_column_name.to_json)["attributes"].sort_by{|s| s["attributes"][@column_name]}
-        @sorted_nested_column
+        @sorted_nested_data = JSON.parse(@nested_column_data)["attributes"].sort_by{|s| s["attributes"][@column_name]}
+        @sorted_nested_data
       end
 
     end
