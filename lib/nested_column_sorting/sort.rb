a3664d2c4cbfd9f03aa879dd0ea9c838c5e2880e
Shilpi89 <shilpi2189@gmail.com>
2014-02-20 03:46:15 -0800

    removed loggers

--- lib/nested_column_sorting/sort.rb
+++ lib/nested_column_sorting/sort.rb
@@ -4,15 +4,18 @@ module Nested_Column
       attr_reader :nested_column_data,:column_name
       def initialize nested_column_data, column_name, options = {}
         @nested_column_data = nested_column_data
-        @column_name = column_name
-        sort_nested_column
+        @column_name = column_name        
       end
 
-      def sort_nested_column
+      def sort_nested_column_asc
         @sorted_nested_data = JSON.parse(@nested_column_data)["attributes"].sort_by{|s| s["attributes"][@column_name]}
-        @sorted_nested_data
+        return @sorted_nested_data.to_a
       end
 
+      def sort_nested_column_dec
+        @sorted_nested_data = JSON.parse(@nested_column_data)["attributes"].sort_by{|s| s["attributes"][@column_name]}.reverse
+        return @sorted_nested_data.to_a
+      end
     end
   end
 end
